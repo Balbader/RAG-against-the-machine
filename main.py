6a73333735d724313052d991f78c5b59554a5aca
BASIL ALBADER <basilalbapro@gmail.com>
2025-09-29 21:18:52 +0200

    testing bm25s

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from ollama import chat
 from ollama import ChatResponse
 from pydantic import BaseModel
+import bm25s
 
 
 class User(BaseModel):
@@ -9,7 +10,6 @@ class User(BaseModel):
 
 
 u = User(id=1, name="basil")
-
 response: ChatResponse = chat(model='qwen3:0.6b', messages=[
     {
         'role': 'user',
@@ -20,3 +20,12 @@ response: ChatResponse = chat(model='qwen3:0.6b', messages=[
 print(u)
 print(response)
 print(response['message']['content'])
+
+corpus = ["cat purrs", "dog barks", "bird sings"]
+tokens = bm25s.tokenize(corpus)
+
+retriever = bm25s.BM25(corpus=corpus, method="bm25+")
+retriever.index(tokens)
+
+docs, scores = retriever.retrieve(bm25s.tokenize("cat"), k=2)
+print(docs, scores)
